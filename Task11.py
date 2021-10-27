18b89d3bda3287a1a0423d3e350ad69754f593df
TMN <super.rikunen@gmail.com>
2021-10-27 11:51:37 +0700

    Initial commit

--- Task11.py
+++ Task11.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import *
-x0 = -2*pi
-xn = 2*pi
+x0 = -pi*2
+xn = pi*2
 
 N = 100
 x = np.linspace(x0, xn, N)
@@ -10,8 +10,8 @@ h = x[1] - x[0]
 A = np.zeros((N, N))
 
 E_main = 10
-E = [0.2]
-Psi = [[1 for arg in x]]
+E = [E_main/10]
+Psi = [[1/sqrt(N) for arg in x]]
 
 
 def U(arg):
@@ -20,30 +20,31 @@ def U(arg):
 
 fig = plt.figure(figsize=(7, 7))
 ax = fig.add_subplot(111)
+plt.title('Task11')
 
 A[N-1, N-1] = 1/h**2 + U(x[N-1])
 for i in range(1, N):
     A[i-1, i-1] = 1/h**2 + U(x[i-1])
     A[i-1, i] = A[i, i-1] = -1/(2*h**2)
-for i in range(1, N):
-    if np.linalg.norm(Psi[i-1]) != 0:
-        Psi.append(np.linalg.solve(A - E[i-1]*np.eye(N), Psi[i-1]))
-        Psi[i] = Psi[i]/np.linalg.norm(Psi[i])
-        E.append(E[i-1] + np.linalg.norm(Psi[i-1])/np.linalg.norm(Psi[i]))
-        print(E[i])
-        if i < 4:
-            ax.plot(x, Psi[i], label='Psi' + str(i))
-        if E[i] < E_main:
-            Psi_main = Psi[i]
-            E_main = E[i]
-    else:
-        N = i
-        break
+
+
+for i in range(1, N+1):
+    Psi.append(np.linalg.solve(A-E[i-1]*np.eye(N, N), Psi[i-1]))
+    E.append(E[i-1] + np.linalg.norm(Psi[i-1])/np.linalg.norm(Psi[i]))
+    #print(E[i])
+    if i < -1:
+        ax.plot(x, Psi[i], label='Psi' + str(i))
+    if E[i] < E_main:
+        Psi_main = Psi[i]
+        E_main = E[i]
 
 print(E_main)
+ax.plot(x, Psi_main, color='r', label='PsiMain')
+#ax.axis([-3.2, 3.2, 0, 1])
 plt.grid()
 plt.legend()
 plt.show()
+
 
 
 
---
+++ Task11_2.py
@@ -0,0 +1,43 @@
+import numpy as np
+from math import *
+import matplotlib.pyplot as plt
+
+N = 100
+
+
+def U(arg):
+    return (arg**2)/2
+
+
+def matrix(X_min, X_max):
+    global h, x, A
+    x = np.linspace(X_min, X_max, N)
+    h = (X_max - X_min)/N
+    A = np.zeros((N, N))
+    A[N-1, N-1] = 1. + U(x[N-1])*h**2
+    for i in range(1, N):
+        A[i-1, i-1] = 1. + U(x[i-1])*h**2
+        A[i, i-1] = A[i-1, i] = -1./2
+
+
+def solve(psi0):
+    psi = [psi0]
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_subplot(1, 1, 1)
+    for i in range(1, N):
+        psi.append(np.linalg.solve(A, psi[i-1]))
+        if i != N-1:
+            psi[i] = psi[i] / (np.linalg.norm(psi[i]))
+    e = np.linalg.norm(psi[N-2])/np.linalg.norm(psi[N-1])
+    print(e/h**2)
+    ax.plot(x, psi[N - 1] / np.linalg.norm(psi[N - 1]), label='Psi Main')
+    plt.title('E='+str(round(e/h**2, 3)))
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+matrix(-pi,pi)
+psi0 = [1/(N**2) for i in range(0, N)]
+solve(psi0)
+
+
